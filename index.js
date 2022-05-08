60ede382754bb467a92210e085dec52c47871a06
ijTuhin <isratishu0209@gmail.com>
2022-05-08 11:18:22 +0600

    installed cors

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require('express');
+const cors = require('cors');
+require('dotenv').config()
 const app = express();
-const port = 5000;
+const port = process.envPORT || 5000;
+
+//middleware
+app.use(cors())
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('Server side!')
