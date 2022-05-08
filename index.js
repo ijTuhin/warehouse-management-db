abf75cfb0d095fb5ada5a260bf605896ca378978
ijTuhin <isratishu0209@gmail.com>
2022-05-08 20:50:48 +0600

    loading all data & individual data using id

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express();
 const port = process.envPORT || 5000;
@@ -10,15 +10,53 @@ app.use(cors());
 app.use(express.json());
 
 
-
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.12dph.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const uri = "mongodb+srv://dbUserTuhin:AYWJHWFEJ6UzILuP@cluster0.12dph.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  console.log('Warehouse Management DB connected');
-  // perform actions on the collection object
-  client.close();
-});
+async function run() {
+    try {
+        await client.connect();
+        const itemCollection = client.db('wareHouse').collection('item');
+        app.get('/item', async(req, res) => {
+            const query = {};
+            const cursor = itemCollection.find(query);
+            const items = await cursor.toArray();
+            res.send(items);
+        });
+
+        app.get('/item/:id', async(req, res) =>{
+            const id = req.params.id;
+            const query={_id: ObjectId(id)};
+            const item = await itemCollection.findOne(query);
+            res.send(item);
+        });
+    }
+    finally {
+
+    }
+};
+run().catch(console.dir);
+
+
+
+/* const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.12dph.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const inventoryCollection = client.db('warehouseDb').collection('inventory');
+        app.get('/inventory', async(req, res) => {
+            const query = {};
+            const cursor = inventoryCollection.find(query);
+            const inventories = await cursor.toArray();
+            res.send(inventories);
+        });
+    }
+    finally {
+
+    }
+};
+run().catch(console.dir); */
 
 
 
