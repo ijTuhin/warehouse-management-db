98bf5d2b1d1b215e2a39c3737336cd7a199c821b
ijTuhin <isratishu0209@gmail.com>
2022-05-09 17:24:30 +0600

    used environment key

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express();
 const port = process.envPORT || 5000;
-
+console.log(process.env);
 //middleware
 app.use(cors());
 app.use(express.json());
 
 
-const uri = "mongodb+srv://dbUserTuhin:AYWJHWFEJ6UzILuP@cluster0.12dph.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.12dph.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 async function run() {
     try {
